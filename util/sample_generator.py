558b778b95df7c48c927397b6503605346124a88
Carlos Santana <carlossantana987@outlook.com>
2019-10-23 11:04:01 -0400

    generation made

--- util/sample_generator.py
+++ util/sample_generator.py
@@ -165,10 +165,13 @@
 
 from django.contrib.auth.models import User
 from adventure.models import Player, Room
+
 Room.objects.all().delete()
+
 size_y = 8
 size_x = 13
 num_rooms = 100
+
 grid = [None] * size_y
 width = size_x
 height = size_y
@@ -178,11 +181,14 @@ for i in range(len(grid)):
 x = -1  # (this will become 0 on the first step)
 y = 0
 room_count = 0
+
 # Start generating rooms to the east
 direction = 1  # 1: east, -1: west
+
 # While there are rooms to be created...
 previous_room = None
 while room_count < num_rooms:
+
     # Calculate the direction of the room to be created
     if direction > 0 and x < size_x - 1:
         room_direction = "e"
@@ -199,7 +205,26 @@ while room_count < num_rooms:
         y += 1
         direction *= -1
     # Create a room in the given direction
-    room = Room(room_count, "A Generic Room", "This is a generic room.", x, y)
+    travel = ""
+    if x > 0 and x < size_x - 1:
+        travel = "east and west."
+    if x == 12 and direction > 0:
+        travel = "north and west."
+    if x == 12 and direction < 0:
+        travel = "south and west."
+    if x == 0 and direction < 0:
+        travel = "north and east."
+    if x == 0 and direction > 0:
+        travel = "south and east."
+    if room_count == 0:
+        travel = "east"
+    if room_count == num_rooms-1:
+        if direction < 0:
+            travel = "east"
+        if direction > 0:
+            travel = "west"
+    room = Room(room_count+1, f"Room #{room_count+1}",
+                f"This is room #{room_count+1}. From here you can travel {travel}", x, y)
     # Note that in Django, you'll need to save the room after you create it
     # Save the room in the World grid
     grid[y][x] = room
